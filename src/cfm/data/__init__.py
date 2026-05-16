2f7f6e5331d62a4de68a567ab7b9721d3eb704df
umaraslam66 <aslamumar16@gmail.com>
2026-05-16 20:58:01 +0200

    chore(data): export vocab_derivation dataclasses

--- src/cfm/data/__init__.py
+++ src/cfm/data/__init__.py
@@ -1,1 +1,19 @@
 """Data pipeline package: Overture loading, tile extraction, validation."""
+
+from cfm.data.vocab_derivation import (
+    FieldPolicy,
+    ListFieldCap,
+    Phase1Policy,
+    Phase1Vocab,
+    SectionDerivation,
+    SectionMetadata,
+)
+
+__all__ = [
+    "FieldPolicy",
+    "ListFieldCap",
+    "Phase1Policy",
+    "Phase1Vocab",
+    "SectionDerivation",
+    "SectionMetadata",
+]
